d5fabaf27ba3b266366e628d56b1039de099d4f8
Elijah Jeruh <elijahgaskinsgeller@gmail.com>
2025-02-06 08:54:33 -0800

    init

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+import * as THREE from 'three';
+
+
+console.log(THREE);
+
+
+let scene = new THREE.Scene();
+
+
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+camera.position.z = 5;
+
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
+function OnWindowResize(e) {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function OnDeviceTilt(e) {
+
+	console.log(e);
+
+	document.getElementById("alpha").textContent = e.alpha;
+	document.getElementById("beta").textContent = e.beta;
+	document.getElementById("gamma").textContent = e.gamma;
+
+	cube.rotation.x = (e.beta * Math.PI) / 180;
+	cube.rotation.y = (e.gamma * Math.PI) / 180;
+	cube.rotation.z = (e.alpha * Math.PI) / 180;
+
+
+
+}
+
+function OnDeviceMotion(e) {
+
+
+	console.log(e);
+
+	document.getElementById("alpha").textContent = e.alpha;
+	document.getElementById("beta").textContent = e.beta;
+	document.getElementById("gamma").textContent = e.gamma;
+
+	cube.rotation.x = (e.beta * Math.PI) / 180;
+	cube.rotation.y = (e.gamma * Math.PI) / 180;
+	//cube.rotation.z = (e.alpha * Math.PI) / 180;
+
+}
+
+
+function animate() {
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
+	renderer.render(scene, camera);
+}
+
+
+
+window.addEventListener("resize", OnWindowResize);
+window.addEventListener("deviceorientation", OnDeviceTilt);
+window.addEventListener("devicemotion", OnDeviceTilt);
+
+renderer.setAnimationLoop(animate);
